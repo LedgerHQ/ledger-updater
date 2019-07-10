555c19aa8f85675d32a6f285484c913004748ad5
meriadec <meriadec.pillet@gmail.com>
2019-07-10 17:39:07 +0200

    Enable copy-paste

--- src/main/menu.js
+++ src/main/menu.js
@@ -7,6 +7,22 @@ export default win => {
       submenu: [{ role: "quit" }],
     },
     {
+      label: "Edit",
+      submenu: [
+        { label: "Undo", accelerator: "CmdOrCtrl+Z", selector: "undo:" },
+        { label: "Redo", accelerator: "Shift+CmdOrCtrl+Z", selector: "redo:" },
+        { type: "separator" },
+        { label: "Cut", accelerator: "CmdOrCtrl+X", selector: "cut:" },
+        { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
+        { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          selector: "selectAll:",
+        },
+      ],
+    },
+    {
       label: "Help",
       submenu: [
         {
